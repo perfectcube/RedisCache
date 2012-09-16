a912e1d4ef58b50b65ff91480083822bdd56891f
Christian Winther <cw@nodes.dk>
2012-09-16 14:17:54 +0200

    remove serialization

--- Lib/Cache/Engine/RedisEngine.php
+++ Lib/Cache/Engine/RedisEngine.php
@@ -35,7 +35,6 @@ class RedisEngine extends CacheEngine {
 		if (!isset($this->redis)) {
 			$this->redis = new Redis();
 			$this->redis->pconnect($this->settings['hostname'], $this->settings['port']);
-			$this->redis->setOption(Redis::OPT_SERIALIZER, Redis::SERIALIZER_PHP);
 		}
 
 		return true;
